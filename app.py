9a11cff07a3bdac98b3d22160b227263e1c353b5
iassist-repindata <repindata@iassistdata.org>
2026-04-20 18:14:10 +0200

    corrected email

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ app_ui = ui.page_sidebar(
                     ),
         ui.nav_panel("About this page", 
                      ui.card(ui.tags.p("Representation in Data aims to identify data sources for minority populations by global region and country. The Data Explorer allows users to explore these sources by keyword, language, resource type, and more."),
-                             ui.tags.div("Report broken links to"),ui.tags.a("repindata@iassistdata.org", href="mailto:repindata@iassist.org"))),
+                             ui.tags.div("Report broken links to"),ui.tags.a("repindata@iassistdata.org", href="mailto:repindata@iassistdata.org"))),
     
         id="tab"
     ),
