241030da218c7ac341706eb529c5d9aa6e267956
iassist-repindata <repindata@iassistdata.org>
2026-09-24 11:25:10 +0200

    Inclusion of Indigenous resources

--- shared.py
+++ shared.py
@@ -5,10 +5,12 @@ import pandas as pd
 app_dir = Path(__file__).parent
 lgbtq = pd.read_csv(app_dir / "data-prep/IASSIST_LGBTIQ_Data.txt", delimiter = "\t", dtype={"PubDate":"Int64", "CollectDateStart":"Int64","CollectDateEnd":"Int64"})
 antir = pd.read_csv(app_dir / "data-prep/IASSIST_Anti_Racism_Data.tsv", delimiter="\t", encoding="utf8", dtype={"PubDate":"Int64", "CollectDateStart":"Int64","CollectDateEnd":"Int64"})
+indig = pd.read_csv(app_dir / "data-prep/IASSIST_Indigenous_Data.tsv", delimiter="\t", encoding="utf8", dtype={"PubDate":"Int64", "CollectDateStart":"Int64","CollectDateEnd":"Int64"})
 
 data_dicty = pd.read_csv(app_dir / "data_dictionary.csv", delimiter=";", encoding="utf8")
+indig["Collection"] = "Indigenous"
 
-df_repindata = pd.concat([lgbtq, antir])
+df_repindata = pd.concat([lgbtq,antir,indig])
 df_repindata.reset_index(drop=True, inplace=True)
 
 # deal with empty cells
